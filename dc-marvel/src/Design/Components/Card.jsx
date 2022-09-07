9dc11bf1aaa4d161086dc69b2b1635a467a9c710
Gilbert Ardila <gilbertferney@gmail.com>
2022-09-07 09:09:12 -0500

    implementamos el lazy loading con Suspense y lazy de react

--- dc-marvel/src/Design/Components/Card.jsx
+++ dc-marvel/src/Design/Components/Card.jsx
@@ -1,17 +1,20 @@
-import React from 'react'
+import React,{Suspense, lazy} from 'react'
 import {Link} from "react-router-dom"
-
+import {Loader} from '../../Heroes/Components/Loader'
  const Card = ({imagen,nombre,id,descripcion}) => {
+  lazy(()=>imagen)
   
   return (
     <div className='w-[250px] h-auto mx-2 my-1 justify-self-center  border-2 border-blue-600  hover:border-solid hover:border-2 hover:border-yellow-300 '
    >
     <div className=' flex flex-col items-center w-full  h-auto text-white'
      >
-      <img src={imagen}
+      <Suspense  fallback={<Loader/>} >
+         <img src={imagen}
       alt={nombre}
-      loading={'lazy'}
       className='w-full h-[350px]'/>
+        </Suspense> 
+     
 
       <p className='bg-gray-400 w-full text-center'>{nombre}</p>
       
